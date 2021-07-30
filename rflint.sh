495d0b78f654b5d2a41cd9a2475817e401b22186
carlosnizolli <55762776+carlosnizolli@users.noreply.github.com>
2021-07-30 16:48:47 -0300

    Update rflint.sh

--- rflint.sh
+++ rflint.sh
@@ -7,6 +7,10 @@ then
 elif ([ ! -z "$2" ] && [ -z "$3" ])
 then
     rflint --recursive --configure $2 $1
+
+elif ([ -z "$2" ] && [ ! -z "$3" ])
+then
+    rflint --recursive --ignore $3 $1
     
 else
     rflint --recursive --configure $2 --ignore $3 $1
