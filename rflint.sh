d826120688695a49b06c026efd21b3979c8e82ef
carlosnizolli <55762776+carlosnizolli@users.noreply.github.com>
2021-08-02 13:08:05 -0300

    Update rflint.sh

--- rflint.sh
+++ rflint.sh
@@ -15,7 +15,7 @@ then
 
 elif ([ ! -z "$2" ] && [ ! -z "$3" ] && [ -z "$4" ])
 then
-    rflint --recursive --configure $2 --ignore $3 $1
+    rflint --recursive --ignore $3 --configure $2 $1
     
 elif [ ! -z "$4" ]
     rflint --argumentfile $4 $1
